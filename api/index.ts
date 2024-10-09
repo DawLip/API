c6a360203e7c8ff6d162af2cf0109e1175429d7e
DawLip <d.lipinski022@gmail..com>
2024-10-09 13:09:23 +0200

    Vercel update

---
+++ api/index.ts
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(express.static('public'));
+
+const path = require('path');
+const port = process.env.PORT
+const uri = process.env.MONGODB;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/', async (req, res) => {
+    let response = {status: "OK"}
+    let data=[]
+    try {
+        const movies = client.db("Researches").collection("Websites Research");
+        
+        const query = {};
+        const options = {};
+
+        const cursor = await movies.find();        
+        for await (const doc of cursor) {
+            data.push(doc)
+          }
+    } finally {await client.close()}
+
+    res.send({...response, data})
+})
+
+app.post('/users-list', (req, res) => {
+    const usersList = req.body;
+    
+    res.send({
+      message: 'New user was added to the list',
+    });
+  });
+
+app.listen(port, () => console.log(`Server ready on port ${port}.`));
+
+module.exports = app;
--- main.ts
+++ main.ts
@@ -1,49 +1,57 @@
-import express from 'express'
+// import express from 'express'
 
-import * as dotenv from 'dotenv'
-import { log } from 'console'
-dotenv.config()
+// import * as dotenv from 'dotenv'
+// import { log } from 'console'
+// dotenv.config()
 
-const app = express()
-const port = process.env.PORT
+// const app = express()
+// const port = process.env.PORT
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB;
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = process.env.MONGODB;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-app.get('/', async (req, res) => {
-    let response = {status: "OK"}
-    let data=[]
-    try {
-        const movies = client.db("Researches").collection("Websites Research");
+// app.get('/', async (req, res) => {
+//     let response = {status: "OK"}
+//     let data=[]
+//     try {
+//         const movies = client.db("Researches").collection("Websites Research");
         
-        const query = {};
-        const options = {};
+//         const query = {};
+//         const options = {};
 
-        const cursor = await movies.find();        
-        for await (const doc of cursor) {
-            data.push(doc)
-          }
-    } finally {await client.close()}
+//         const cursor = await movies.find();        
+//         for await (const doc of cursor) {
+//             data.push(doc)
+//           }
+//     } finally {await client.close()}
 
-    res.send({...response, data})
-})
+//     res.send({...response, data})
+// })
 
-app.post('/users-list', (req, res) => {
-    const usersList = req.body;
+// app.post('/users-list', (req, res) => {
+//     const usersList = req.body;
     
-    res.send({
-      message: 'New user was added to the list',
-    });
-  });
+//     res.send({
+//       message: 'New user was added to the list',
+//     });
+//   });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
