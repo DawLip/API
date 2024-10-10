ee76052b8812bf0cfd02d6e3f1d6faf1b3f18eab
DawLip <d.lipinski022@gmail..com>
2024-10-10 07:26:50 +0200

    Add coments and posibility to switch database and collection in api

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express'
 
 import cors from 'cors'
 import { MongoClient, ServerApiVersion } from 'mongodb'
+import { log } from 'console';
 
 const app = express()
 
@@ -22,38 +23,64 @@ const serverApi = {
   }
 
 app.get('/', async (req: Request, res: Response) => {
-    let response = {status: "OK", data: []}
-    let data=[]
+    /* 
+    req.query: Not used
+
+    req.body: Object
+        database: String <= mongoDB database name
+        collection: String <= mongoDB collection name
+
+    return: Object
+        data: [{any}] <= data from database
+    */
+
     const client = new MongoClient(uri, {serverApi});
-
+    let response = {data: []}
+    let data=[]
+    const dbName = String(req.query?.database || "Researches")
+    const colName =String(req.query.collection || "Websites Research")
+    const query = {};
+    const options = {};
+    
     try {
-        const collection = client.db("Researches").collection("Websites Research");
-        
-        const query = {};
-        const options = {};
-
+        const collection = client.db(dbName).collection(colName);
         const cursor = await collection.find();        
         for await (const doc of cursor) {
             data.push(doc)
           }
     } finally {await client.close()}
 
-    res.send({...response, data})
+    res.status(200).send({...response, data})
 })
 
 
 app.post('/', async (req: Request, res: Response) => {
-    const data = req.body;
-    let response = {status: "OK", data: []}
+    /* 
+    req.query: Not used
+
+    req.body:
+        {
+            options: Object
+                database: String <= mongoDB database name
+                collection: String <= mongoDB collection name
+            data:
+                [{any},]
+        }
+
+    return: Not used
+    */
+
     const client = new MongoClient(uri, {serverApi});
+    let response = {}
+    const data = req.body;
     
     try {
-        const collection = client.db("Researches").collection("Websites Research");
+        const collection = client.db(data.options?.db||"Researches").collection(data.options?.collection||"Websites Research");
         
         await collection.insertOne(data);        
     } finally {await client.close()}
 
-    res.send(response);
+    res.status(200).send({...response});
   });
 
 app.listen(port, () => console.log(`Server ready on port ${port}.`));
